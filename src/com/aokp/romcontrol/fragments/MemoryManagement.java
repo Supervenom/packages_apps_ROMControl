f878490cfbe896a88e1e21010cc6a13dd445a838
Gabriele <vanonigabriele@gmail.com>
2012-05-27 20:58:44 +0200

    zram

--- src/com/aokp/romcontrol/fragments/MemoryManagement.java
+++ src/com/aokp/romcontrol/fragments/MemoryManagement.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.aokp.romcontrol.fragments;
 
 import java.io.File;
@@ -14,7 +30,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-public class zRam extends AOKPPreferenceFragment implements
+public class MemoryManagement extends AOKPPreferenceFragment implements
         OnPreferenceChangeListener {
 
     public static final String KSM_RUN_FILE = "/sys/kernel/mm/ksm/run";
